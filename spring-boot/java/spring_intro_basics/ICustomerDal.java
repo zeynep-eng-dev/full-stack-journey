9597280bbf44e486fe48870c39190d7b0f207eca
ZZEYNEP <zeynepp.nuh22@gmail.com>
2026-08-02 14:59:51 +0300

    Delete spring-boot directory

--- spring-boot/java/spring_intro_basics/ICustomerDal.java
+++
@@ -1,5 +0,0 @@
-package spring_intro_basics;
-
-public interface ICustomerDal {
-	 public void add() ;
-}
--- spring-boot/java/spring_intro_basics/oracleCustomerDal.java
+++
@@ -1,8 +0,0 @@
-package spring_intro_basics;
-
-public class oracleCustomerDal implements ICustomerDal {
-	public void add() {
-		System.out.println(" oracle veri tabanına eklendi");
-	}
-
-}
